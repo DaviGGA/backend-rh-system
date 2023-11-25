126f39d434deab0f5f0d3a893db616d49d08349e
Davi <davigregorio.a@hotmail.com>
2023-11-25 13:30:03 -0300

    cors

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const employeeRouter = require('./employee/router');
 const pointRouter = require('./point/router');
 const userRouter = require('./user/router');
 const messageRouter = require('./message/router');
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors();)
 
 app.use(
     express.urlencoded({
