5ad1bf293710d2ae37a4f86df623d491bf991100
Davi <davigregorio.a@hotmail.com>
2023-11-25 13:31:11 -0300

    fix bug

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const messageRouter = require('./message/router');
 const cors = require('cors');
 
 app.use(express.json());
-app.use(cors();)
+app.use(cors());
 
 app.use(
     express.urlencoded({
